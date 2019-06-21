fdceebfbc0bd64ff9aef718bb3106ef4c4cd99da
Marcus Sanatan <msanatan@gmail.com>
2019-06-21 01:34:18 -0400

    Closer match the order of the article

--- game.js
+++ game.js
@@ -26,29 +26,29 @@ function preload() {
   // Image layers from Tiled can't be exported to Phaser 3 (as yet)
   // So we add the background image separately
   this.load.image('background', 'assets/images/background.png');
-  // Load the tileset image file, needed for the map to know what
-  // tiles to draw on the screen
-  this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
   // Even though we load the tilesheet with the spike image, we need to
   // load the Spike image separately for Phaser 3 to render it
   this.load.image('spike', 'assets/images/spike.png');
   // Load the export Tiled JSON
-  this.load.tilemapTiledJSON('map', 'assets/tilemaps/level1.json');
   // Load player animations from the player spritesheet and atlas JSON
   this.load.atlas('player', 'assets/images/kenney_player.png',
     'assets/images/kenney_player_atlas.json');
+  // Load the tileset image file, needed for the map to know what
+  // tiles to draw on the screen
+  this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
+  this.load.tilemapTiledJSON('map', 'assets/tilemaps/level1.json');
 }
 
 function create() {
+  // Place the background image in our game world
+  const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
+  // Scale the image to better match our game's resolution
+  backgroundImage.setScale(2, 0.8);
   // Create a tile map, which is used to bring our level in Tiled
   // to our game world in Phaser
   const map = this.make.tilemap({ key: 'map' });
   // Add the tileset to the map so the images would load correctly in Phaser
   const tileset = map.addTilesetImage('kenney_simple_platformer', 'tiles');
-  // Place the background image in our game world
-  const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
-  // Scale the image to better match our game's resolution
-  backgroundImage.setScale(2, 0.8);
   // Add the platform layer as a static group, the player would be able
   // to jump on platforms like world collisions but they shouldn't move
   const platforms = map.createStaticLayer('Platforms', tileset, 0, 200);
